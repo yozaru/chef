3e182534359eb597a5062a5c436c6220e9a64d38
root <root@ip-172-31-40-141.us-west-2.compute.internal>
2016-01-26 14:36:22 +0900

    bind-utlis add

---
+++ site-cookbooks/bind-utils/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: bind-utils
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "bind-utils" do
+ action :install
+end
